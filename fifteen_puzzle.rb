dd8aba0159957d66580341e5c4ef866e8c747202
Rahul Horé <hore.rahul@gmail.com>
2014-07-03 15:47:42 -0400

    Finds Manhattan distance of all tiles

--- fifteen_puzzle.rb
+++ fifteen_puzzle.rb
@@ -20,6 +20,27 @@ def solvable?(grid)
   ((grid_size.even? && blanks_row.odd?) == inversions.even?)
 end
 
+# Manhattan: Take the sum of the absolute values of the differences of the coordinates.
+
+# For example, if x=(a,b) and y=(c,d), the Manhatten distance between x and y is |a−c|+|b−d|.
+
+def manhattan_distance(grid)
+  metric = grid
+  grid_size = grid.size
+  metric.each.with_index do |row, r|
+    row.map!.with_index do |tile, c|
+      if tile == 0
+        0
+      else
+        target_row = ((tile - 1) / grid_size) + 1
+        target_col = tile % grid_size
+        target_col = 4 if target_col == 0
+        (r + 1 - target_row).abs + (c + 1 - target_col).abs
+      end
+    end
+  end
+end
+
 puz = [[12,  1, 10,  2],
        [ 7, 11,  4, 14],
        [ 5,  0,  9, 15],
@@ -33,3 +54,5 @@ insoluble =
 
 p solvable?(puz)
 p solvable?(insoluble)
+
+p manhattan_distance(puz)
