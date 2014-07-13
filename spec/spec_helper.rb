8c9a8bea761058cbd10c10ae5c46c24703410976
Rahul Horé <hore.rahul@gmail.com>
2014-07-13 19:47:53 -0400

    Fixes spec_helper.rb
    Apparently `config.color_enabled` is just `config.color` in 3.0+.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
   # Use color in STDOUT
-  config.color_enabled = true
+  config.color = true
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
   # Use the specified formatter
