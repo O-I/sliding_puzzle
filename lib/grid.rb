1f0e81b91183aad5a36262559c952090a199e6d5
Rahul Horé <hore.rahul@gmail.com>
2014-07-19 20:13:54 -0400

    Adds #blank_at_column

--- lib/grid.rb
+++ lib/grid.rb
@@ -85,6 +85,10 @@ module SlidingPuzzle
       tiles.index(0) / height
     end
 
+    def blank_at_column
+      tiles.index(0) % width
+    end
+
     def inversions
       tiles.map.with_index do |tile, pos|
         tiles[pos...grid_size].count do |t|
