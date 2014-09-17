46cf047fed9eb34b3704de434b1a3b9af8e06cb6
Rahul Horé <hore.rahul@gmail.com>
2014-09-17 17:14:59 -0400

    Profiles performance issues

--- sliding_puzzle.rb
+++ sliding_puzzle.rb
@@ -1,67 +1,9 @@
-# Algorithms for determining the solvability of sliding 15 puzzle
+require 'ruby-prof'
+require_relative 'lib/grid'
 
-# Determines solvability of an nxn puzzle
-# See this for basic algorithm:
-# http://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+puzzle = SlidingPuzzle::Grid.new [[6,4,3],[1,0,2],[8,5,7]]
 
-def solvable?(grid)
-  state = grid.flatten
-  grid_size = grid.size
-  state_size = grid_size ** 2
-  blanks_row = state.index(0) / grid_size
+result = RubyProf.profile { puzzle.solve }
 
-  inversions = state.map.with_index do |tile, pos|
-    state[pos..state_size - 1].count do |t|
-      0 < t && t < tile
-    end
-  end.reduce(:+)
-
-  (grid_size.odd?  && inversions.even?) ||
-  ((grid_size.even? && blanks_row.odd?) == inversions.even?)
-end
-
-# Manhattan: Take the sum of the absolute values of the differences of the coordinates.
-
-# For example, if x=(a,b) and y=(c,d), the Manhatten distance between x and y is |a−c|+|b−d|.
-
-def manhattan_distance(grid)
-  grid_size = grid.size
-  grid.map.with_index do |row, r|
-    row.map.with_index do |tile, c|
-      if tile == 0
-        0
-      else
-        target_row = ((tile - 1) / grid_size) + 1
-        target_col = tile % grid_size
-        target_col = grid_size if target_col == 0
-        (r + 1 - target_row).abs + (c + 1 - target_col).abs
-      end
-    end
-  end
-end
-
-def hamming_weight(grid)
-  grid_size = grid.size
-  grid.flatten.map.with_index do |tile, i|
-    tile == 0 || tile == i + 1 ? 0 : 1
-  end.each_slice(grid_size).to_a
-end
-
-puz = [[12,  1, 10,  2],
-       [ 7, 11,  4, 14],
-       [ 5,  0,  9, 15],
-       [ 8, 13,  6,  3]]
-
-insoluble =
-      [[ 1,  2,  3,  4],
-       [ 5,  6,  7,  8],
-       [ 9, 10, 11, 12],
-       [13, 15, 14,  0]]
-
-p solvable?(puz)
-p solvable?(insoluble)
-
-p manhattan_distance(puz)
-p puz
-p hamming_weight(puz)
-p hamming_weight(insoluble)
+printer = RubyProf::GraphPrinter.new(result)
+printer.print(STDOUT, {})
